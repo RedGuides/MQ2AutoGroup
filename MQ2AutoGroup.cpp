8caf74d4c8a0c66ff77bf4d7356d7c5de581fe56
James Hateley <ask@redguides.com>
2017-09-23 21:57:01 -0500

    test source! we can compile again

--- MQ2AutoGroup.cpp
+++ MQ2AutoGroup.cpp
@@ -1280,10 +1280,10 @@ PLUGIN_API VOID OnPulse(VOID)
 	{
 		if (((PCSIDLWND)(pWnd))->dShow)
 		{
-			if (CXWnd *Child = pWnd->GetChildItem("CD_TextOutput"))
+			if (CStmlWnd*Child = (CStmlWnd*)pWnd->GetChildItem("CD_TextOutput"))
 			{
 				char ConfirmationText[MAX_STRING];
-				GetCXStr(((PCSIDLWND)Child)->SidlText, ConfirmationText, sizeof(ConfirmationText));
+				GetCXStr(Child->STMLText, ConfirmationText, sizeof(ConfirmationText));
 				if (strstr(ConfirmationText, "If you accept"))
 				{
 					if (strstr(ConfirmationText, "you and your group members will lose"))
